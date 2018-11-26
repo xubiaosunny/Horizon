c933405c5b63df566d88ffb909a0ce29d3445a65
xubiao <xubiaosunny@163.com>
2018-11-26 10:48:35 +0800

    format

--- go/src/server/wallpaper/bing.go
+++ go/src/server/wallpaper/bing.go
@@ -1,38 +1,38 @@
 package wallpaper
 
 import (
-
 	"database/sql"
-	"net/http"
-	"github.com/gin-gonic/gin"
-	"io/ioutil"
-	"path"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"path"
+
+	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 type WapllpStruct struct {
-    Date string
-	FileName string
+	Date      string
+	FileName  string
 	Copyright string
 }
 
 func checkErr(err error) {
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
 
-func Bing (c *gin.Context) {
+func Bing(c *gin.Context) {
 	idx := c.DefaultQuery("idx", "0")
 	n := c.DefaultQuery("n", "10")
 
 	bingPath := "../../../dist/bing"
 	data := []WapllpStruct{}
 
-	db, err := sql.Open("sqlite3", bingPath + "/bing.db")
+	db, err := sql.Open("sqlite3", bingPath+"/bing.db")
 	checkErr(err)
-	
+
 	stmt, err := db.Prepare("SELECT * FROM cn_bing order by date desc limit ?, ?;")
 	checkErr(err)
 
@@ -52,18 +52,17 @@ func Bing (c *gin.Context) {
 		checkErr(err)
 	}
 
-
 	files, _ := ioutil.ReadDir(bingPath)
 	fmt.Println(reverse(files))
-    for _, file := range files {
-        if file.IsDir() {
-            continue
-        } else {
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		} else {
 			fileSuffix := path.Ext(file.Name())
-			if (fileSuffix == ".jpg") {
+			if fileSuffix == ".jpg" {
 				data = append(data, WapllpStruct{fileSuffix, file.Name()})
 			}
-        }
+		}
 	}
 	fmt.Println(data)
 	c.JSON(http.StatusOK, data)
